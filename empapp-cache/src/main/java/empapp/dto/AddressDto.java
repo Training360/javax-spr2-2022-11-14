6e2823c0d5be6dde000f6a46607937c392f2114b
Viczián István <viczian.istvan@gmail.com>
2022-11-14 22:06:49 +0100

    Redis konfigurálása

--- empapp-cache/src/main/java/empapp/dto/AddressDto.java
+++ empapp-cache/src/main/java/empapp/dto/AddressDto.java
@@ -1,10 +1,11 @@
 package empapp.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
-public class AddressDto {
+public class AddressDto implements Serializable {
 
     private Long id;
 
--- empapp-cache/src/main/java/empapp/dto/EmployeeDto.java
+++ empapp-cache/src/main/java/empapp/dto/EmployeeDto.java
@@ -2,10 +2,11 @@ package empapp.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class EmployeeDto {
+public class EmployeeDto implements Serializable {
 
     private Long id;
 

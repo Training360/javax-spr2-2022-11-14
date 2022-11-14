ff4fe05b03fcea5f2c1836fe68b923ccc71d19b2
Viczián István <viczian.istvan@gmail.com>
2022-11-14 22:05:01 +0100

    Paraméter neve ha elveszik fordításkor

--- empapp-cache/src/main/java/empapp/EmployeeRepository.java
+++ empapp-cache/src/main/java/empapp/EmployeeRepository.java
@@ -3,6 +3,7 @@ package empapp;
 import empapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +14,5 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findAllWithAddresses();
 
     @Query("select distinct e from Employee e left join fetch e.addresses where e.id = :id")
-    Optional<Employee> findByIdWithAddresses(Long id);
+    Optional<Employee> findByIdWithAddresses(@Param("id") Long id);
 }

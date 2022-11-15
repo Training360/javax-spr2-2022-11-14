81a5e9d067c146b56716b3575bfb5819eac392cf
Viczián István <viczian.istvan@gmail.com>
2022-11-15 13:02:59 +0100

    WebSocket init

---
+++ empapp-websocket/src/main/java/empapp/EmployeeService.java
@@ -0,0 +1,49 @@
+package empapp;
+
+import empapp.dto.CreateEmployeeCommand;
+import empapp.dto.EmployeeDto;
+import empapp.dto.UpdateEmployeeCommand;
+import empapp.entity.Employee;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    private EmployeeMapper employeeMapper;
+
+    public EmployeeDto createEmployee(CreateEmployeeCommand command) {
+        Employee employee = employeeMapper.toEmployee(command);
+        employeeRepository.save(employee);
+        return employeeMapper.toEmployeeDto(employee);
+    }
+
+    public List<EmployeeDto> listEmployees() {
+        return employeeMapper.toEmployeesDto(employeeRepository.findAllWithAddresses());
+    }
+
+    public EmployeeDto findEmployeeById(long id) {
+        return employeeMapper.toEmployeeDto(employeeRepository.findByIdWithAddresses(id)
+                        .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id)));
+    }
+
+    @Transactional
+    public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
+        Employee employeeToModify = employeeRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id));
+        employeeToModify.setName(command.getName());
+        return employeeMapper.toEmployeeDto(employeeToModify);
+    }
+
+    public void deleteEmployee(long id) {
+        Employee employee = employeeRepository.findByIdWithAddresses(id)
+                .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id));
+        employeeRepository.delete(employee);
+    }
+}
---
+++ empapp-websocket/src/main/java/empapp/dto/AddressDto.java
@@ -0,0 +1,12 @@
+package empapp.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class AddressDto {
+
+    private Long id;
+
+    private String city;
+}

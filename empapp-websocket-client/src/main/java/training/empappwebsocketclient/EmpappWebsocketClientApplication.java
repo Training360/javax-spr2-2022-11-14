6552590f719862c8de45c1c065868dba40aef8d1
Viczián István <viczian.istvan@gmail.com>
2022-11-15 15:11:29 +0100

    WebSocket client init

---
+++ empapp-websocket-client/src/main/java/training/empappwebsocketclient/EmpappWebsocketClientApplication.java
@@ -0,0 +1,13 @@
+package training.empappwebsocketclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpappWebsocketClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpappWebsocketClientApplication.class, args);
+	}
+
+}
---
+++ empapp-websocket-client/src/test/java/training/empappwebsocketclient/EmpappWebsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package training.empappwebsocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpappWebsocketClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

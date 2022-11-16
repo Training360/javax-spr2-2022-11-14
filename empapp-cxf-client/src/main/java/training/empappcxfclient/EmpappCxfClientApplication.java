821bf4fda8ce7a46c5c7197367d2426f480ec88d
Viczián István <viczian.istvan@gmail.com>
2022-11-16 14:27:00 +0100

    CFX client init

---
+++ empapp-cxf-client/src/main/java/training/empappcxfclient/EmpappCxfClientApplication.java
@@ -0,0 +1,13 @@
+package training.empappcxfclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpappCxfClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpappCxfClientApplication.class, args);
+	}
+
+}
---
+++ empapp-cxf-client/src/test/java/training/empappcxfclient/EmpappCxfClientApplicationTests.java
@@ -0,0 +1,13 @@
+package training.empappcxfclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpappCxfClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

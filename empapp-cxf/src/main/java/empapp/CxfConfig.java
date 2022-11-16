f52d37619f8a5fc72982551fb32f49ffa7a35de0
Viczián István <viczian.istvan@gmail.com>
2022-11-16 16:26:55 +0100

    Cxf logging

--- empapp-cxf/src/main/java/empapp/CxfConfig.java
+++ empapp-cxf/src/main/java/empapp/CxfConfig.java
@@ -1,6 +1,8 @@
 package empapp;
 
 
+import org.apache.cxf.ext.logging.LoggingFeature;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,13 +10,25 @@ import org.apache.cxf.Bus;
 import javax.xml.ws.Endpoint;
 import org.apache.cxf.jaxws.EndpointImpl;
 
+import java.util.List;
+
 @Configuration
 public class CxfConfig {
 
     @Bean
     public Endpoint employeeEndpoint(Bus bus, EmployeesWebService employeesWebService) {
         var endpoint = new EndpointImpl(bus, employeesWebService);
+        endpoint.setFeatures(List.of(loggingFeature()));
         endpoint.publish("/employees");
         return endpoint;
+    }
+
+    @Bean
+    public LoggingFeature loggingFeature() {
+        var loggingFeature = new LoggingFeature();
+        loggingFeature.setPrettyLogging(true);
+        loggingFeature.setVerbose(true);
+        loggingFeature.setLogMultipart(true);
+        return loggingFeature;
     }
 }

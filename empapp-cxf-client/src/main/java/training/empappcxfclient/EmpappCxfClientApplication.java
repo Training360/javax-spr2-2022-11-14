e8fcbbc4bd93d9a06297a097767fe6328c175be0
Viczián István <viczian.istvan@gmail.com>
2022-11-16 14:51:28 +0100

    Kliens implementáció

--- empapp-cxf-client/src/main/java/training/empappcxfclient/EmpappCxfClientApplication.java
+++ empapp-cxf-client/src/main/java/training/empappcxfclient/EmpappCxfClientApplication.java
@@ -1,13 +1,28 @@
 package training.empappcxfclient;
 
+import empapp.EmployeesWebServiceService;
+import empapp.FindEmployeeByIdWdto;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmpappCxfClientApplication {
+public class EmpappCxfClientApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpappCxfClientApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		var service = new EmployeesWebServiceService();
+		var port = service.getEmployeesWebServicePort();
+		var request = new FindEmployeeByIdWdto();
+		request.setId(1);
+
+		var response = port.findEmployeeById(request);
+		System.out.println(response.getId() + " " + response.getName());
+
+	}
 }
